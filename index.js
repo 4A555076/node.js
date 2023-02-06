862651c8f8d7848e9df43863c2e34efcda86f423
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-06 10:58:39 +0800

    index

--- index.js
+++ index.js
@@ -1,17 +1,230 @@
-require('dotenv').config();
+if(process.argv[2]==='production'){
+  require('dotenv').config({path:__dirname+ '/production.env'});
+}else{
+  require('dotenv').config({path:__dirname+ '/dev.env'});
+};
 
+const bcrypt = require('bcryptjs')
+const multer = require('multer');
+const upload = require('./modules/upload-img');
+const session = require('express-session');  //session 放最前面(注意順序!)
+const MYsqlStore = require('express-mysql-session')(session);
+const moment = require('moment-timezone');
+const db = require('./modules/connect-mysql');
+const sessionStore = new MYsqlStore({}, db);  //{}->放連線的帳號和密碼，因已設定，所以不用放
 const express = require('express');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 //路由設定,routes
+
+const corsOptions = {
+  credentials:true,
+  origin: (origin, callback)=>{
+    console.log({origin});
+    callback(null, true);
+  },
+};
+app.use(require('cors')(corsOptions));
+
+//top-level middleware 
+//解析cookie，拿到sessionId，再把session資料放到req.session
+app.use(session({
+  saveUninitialized: false, //session尚未初始化時 是否存起來(與儲存媒介有關)
+  resave: false,  //沒變更內容是否強制回存
+  secret: 'skdjskdakslkdjlkflqwlkelkdjs', //加密
+  store: sessionStore,
+  // cookie:{
+  //   maxAge: 1200_000    //存活20分鐘
+  // }   //瀏覽器持續開著，session基本上一直存活
+}));
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+app.use((req, res, next)=>{
+  res.locals.title = process.env.SITE_TITLE || "*** 沒有設定 ***";
+  //res.locals =>進到template
+  //掛在locals底下的屬性會變成template裡的全域變數
+
+  //樣板輔助函式 helper function
+  res.locals.toDateString = d=>moment(d).format('YYYY-MM-DD');
+  res.locals.toDatetimeString = d=>moment(d).format('YYYY-MM-DD HH:mm:ss');
+  //直接將session 資料放到locals(res.locals.sessiond)，讓template可以吃到session
+  res.locals.session = req.session;
+  next();   //若要往下傳->必須呼叫next()
+  })
+
 app.get("/",(req,res) => {
     res.render('main' , {name:'專題'});
 });
 
 app.use(express.static('node_modules/bootstrap/dist'));
+
+
+//member.json
+app.get('/memberlist',(req,res)=>{
+  const data = require(__dirname + '/data/member.json');
+  //require 可以在程式的任何地方使用
+  console.log(data); //取得已經是原生類型
+  // res.json(data);  
+  res.render('memberlist',{data});
+});
+
+//取得queryString資料
+//可重複給值 (變成陣列)
+app.get('/try-qs',(req,res)=>{
+  res.json(req.query);  
+});
+
+app.post(['/try-post','/try-post2'], (req,res)=>{
+  res.json(req.body);  
+});
+//若沒使用urlencodedParser幫忙處理，req.body為undefined
+//根據檔頭來判斷 資料進來時是否要運作
+
+app.get('/try-post-form', (req,res)=>{
+  res.render('try-post-form');
+});
+
+app.post('/try-post-form', (req,res)=>{
+  // res.json(req.body);
+  res.render('try-post-form', req.body);
+});
+
+app.post('/try-upload', upload.single('avatar'), (req,res)=>{
+  res.json(req.file); //上傳單一檔案
+}); 
+
+app.post('/try-uploads', upload.array('photos'), (req,res)=>{
+  res.json(req.files); //上傳多個檔案
+}); 
+
+//寬鬆規則
+app.get('/my-params1/:action?/:id?', (req,res)=>{
+  res.json(req.params); 
+}); 
+
+//手機號碼
+app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req,res)=>{
+  let u = req.url.slice(3);  // 從第三個以後開始顯示
+  u = u.split('?')[0];       //丟掉query String
+  u = u.split('-').join(''); //去掉減號
+  res.send(u); 
+}); 
+
+
+//嚴謹規則(因放於寬鬆規則後，此路由永遠無法拜訪)
+app.get('/my-params1/abc', (req,res)=>{
+  res.json(req.params); 
+}); 
+//!!越寬鬆規則放越後面，嚴謹放前面
+
+//可同時把router掛在不同baseUrl底下
+app.use(require('./routes/admin2')); 
+app.use('/admins', require('./routes/admin2'));
+// app.use('/admins-new', require('./routes/new')); //切換版本(admin3做法)
+
+app.get('/try-sess', (req, res)=>{
+  req.session.my_var = req.session.my_var || 0;  //預設為0
+  req.session.my_var ++;        
+  res.json({
+    my_var: req.session.my_var, 
+    session: req.session          //session不要取名為cookie
+  }) 
+});
+
+app.get('/try-moment', (req, res)=>{
+  const d1 = new Date();
+ const m1 = moment();  //new Date()
+ const m1a = m1.format('YYYY/MM/DD'); 
+ const m1b = m1.format('YYYY-MM-DD HH:mm:ss'); 
+ const m1c = m1.tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm:ss');
+ const m2 = moment('2023-01-05');  //new Date()
+ res.json({m1a, m1b, d1, m1c, m2});
+});
+
+//新增會員資料
+app.get('/add-member', async (req, res)=>{
+  return res.json({}); //已新增就不要再新增了
+  const sql = "INSERT INTO `member`(`name`, `email`, `password`, `hash`, `created_at`) VALUES ('tester', ?, ?, '', Now())";
+  const password = await bcrypt.hash('13579',10);
+  const [result] = await db.query(sql,['tester@test.com', password]);
+  
+  res.json(result);
+  });
+//登入
+//呈現登入表單
+app.get('/login', async (req, res)=>{
+  return res.render('login');   
+});
+//處理登入後，有無email、password這兩值
+app.post('/login', upload.none(), async (req, res)=>{
+  const output ={
+    success:false,
+    code:0,
+    error:'',
+  };
+  const {email, password} = req.body;
+  if(!email | !password){  //若email或密碼 其中一個沒有
+    output.error = '欄位資料不足'
+    output.code = 400;
+    return res.json(output);
+  }
+  const sql = "SELECT * FROM  member WHERE email =?";
+  const [rows] = await db.query(sql,[email]); //上方的? => [email]
+  if(rows.length < 1){
+    output.error = '信箱錯誤'
+    output.code = 410;
+    return res.json(output);
+  }
+   const row = rows[0];
+   
+   //密碼比對
+   const result = await bcrypt.compare(password,row.password);
+   if(result){
+    output.success = true;
+    //成功登入->設定session
+    req.session.user={
+      id:row.id,
+      email,  //=>[email]
+      name : row.name
+    };
+   }else{
+    output.error = "密碼錯誤";
+    output.code = 420;
+   }
+
+  return res.json(output);   
+});
+
+//登出
+app.get('/logout', async (req, res)=>{
+   delete req.session.user; //刪掉session
+   return res.redirect('/');  //轉向首頁
+});
+
+//不透過表單登入流程(不使用帳密)
+//快速登入
+app.get('/fake1', async (req, res)=>{
+  req.session.user={
+    id:1,
+    email:'test123@test.com',
+    name : '管理者'  //密碼: 24680
+  };
+  return res.redirect('/');  //轉向首頁
+});
+
+//baseUrl
+app.use('/member', require('./routes/member'));
+
 app.use(express.static('public'));
+//*****所有路由設定都要放在這行之前*****
+app.use((req,res)=>{
+  res.type('text/html');
+  res.status(404).send(`<h1>404找不到你要的頁面</h1>`);
+}); 
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
