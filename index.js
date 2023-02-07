8e1cfb78b0b9ee69f8605607f93e6ed08f6dba73
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-07 11:00:34 +0800

    註冊會員功能 ; 增加nodemailer 套件

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ if(process.argv[2]==='production'){
 const bcrypt = require('bcryptjs')
 const multer = require('multer');
 const upload = require('./modules/upload-img');
+const nodemailer = require('nodemailer');
 const session = require('express-session');  //session 放最前面(注意順序!)
 const MYsqlStore = require('express-mysql-session')(session);
 const moment = require('moment-timezone');
@@ -60,7 +61,7 @@ app.get("/",(req,res) => {
     res.render('main' , {name:'專題'});
 });
 
-app.use(express.static('node_modules/bootstrap/dist'));
+// app.use(express.static('node_modules/bootstrap/dist'));
 
 
 //member.json
@@ -145,6 +146,68 @@ app.get('/try-moment', (req, res)=>{
  res.json({m1a, m1b, d1, m1c, m2});
 });
 
+
+//註冊
+app.get('/register', (req,res)=>{
+  res.render('register');
+});
+
+// /auth/register
+app.post("/auth/register", upload.none(), async(req, res) => {    
+  const output = {
+    success:false,
+    postData: req.body, //除錯用
+    code:0,
+    errors: {}
+  };
+
+  let {name, email, mobile, birthday, address, password, password_checked } = req.body;
+
+  if(!name || name.length<2 ){
+    output.errors.name='請輸入正確的姓名';
+    return res.json(output);   //輸出，但後面不執行時->加return
+  }
+  birthday = moment(birthday);
+  birthday = birthday.isValid() ? birthday.format('YYYY-MM-DD') : null;  
+
+  const sql = "SELECT email FROM member WHERE email=?";
+  const [rows] = await db.query(sql,[email]);
+  if(rows.length > 0 ) {
+    output.error = '信箱已註冊過!'
+    return res.json(output)
+  }
+
+  if(password !== password_checked) {
+    output.error = '密碼不一致!'
+    return res.json(output)
+  }
+
+
+  let hashedPassword = await bcrypt.hash(password, 10)
+
+  console.log(hashedPassword);
+     
+  const newSQL = "INSERT INTO `member`(`name`, `email`,`mobile`, `birthday`, `address`,`password`,`created_at`)VALUES(?, ?, ?, ?, ?, ?,NOW())";
+
+  const [result] = await db.query( newSQL, [name, email , mobile, birthday, address, hashedPassword]);
+
+  output.result = result;
+  output.success = !!result.affectedRows;
+
+  return res.json(output); 
+
+  // if(error) {
+  //   console.log(error)
+  //   } else {
+  //   return res.render('register', {
+  //   message: 'User registered!'
+  //   })
+  //   }
+  })        
+  
+
+
+
 //新增會員資料
 app.get('/add-member', async (req, res)=>{
   return res.json({}); //已新增就不要再新增了
@@ -159,6 +222,7 @@ app.get('/add-member', async (req, res)=>{
 app.get('/login', async (req, res)=>{
   return res.render('login');   
 });
+
 //處理登入後，有無email、password這兩值
 app.post('/login', upload.none(), async (req, res)=>{
   const output ={
@@ -223,7 +287,7 @@ app.use(express.static('public'));
 //*****所有路由設定都要放在這行之前*****
 app.use((req,res)=>{
   res.type('text/html');
-  res.status(404).send(`<h1>404找不到你要的頁面</h1>`);
+  res.status(404).send(`<h1>404找不到你要的頁面yoyoy</h1>`);
 }); 
 
 const port = process.env.PORT || 3001;
