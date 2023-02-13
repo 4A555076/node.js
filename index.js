4b7df1ae55fc2ab68715c1c84f7bca988daea40c
Chu-Ya Hsu <t199511137@gmail.com>
2023-02-13 15:10:28 +0800

    更改status(文字內容)

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ app.use(express.static('public'));
 //*****所有路由設定都要放在這行之前*****
 app.use((req,res)=>{
   res.type('text/html');
-  res.status(404).send(`<h1>404找不到你要的頁面yoyoy</h1>`);
+  res.status(404).send(`<h1>404找不到你要的頁面</h1>`);
 }); 
 
 const port = process.env.PORT || 3001;
