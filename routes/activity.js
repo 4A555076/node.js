c5934f75456aafedb6f5754bf28dcc821d1e14b7
4A555076 <fz555076@gmail.com>
2023-03-12 01:05:55 +0800

    feat : 活動新增多張照片

--- routes/activity.js
+++ routes/activity.js
@@ -82,12 +82,18 @@ const getListData = async(req,res)=>{
     return {totalRows,totalPages,page,rows};
 }
 
+router.post("/upload-img",upload.array("images"),async(req,res)=>{
+    res.json(req.files);
+  });
+router.post("/upload-img",upload.array("images"),async(req,res)=>{
+res.json(req.files);
+});
 
 router.get("/add",async(req,res)=>{
     res.render("activity-add");
 });
 
-router.post("/add",upload.single("activity_image"),async(req,res)=>{
+router.post("/add",upload.none(),async(req,res)=>{
 
     const output = {
         success:false,
@@ -96,9 +102,9 @@ router.post("/add",upload.single("activity_image"),async(req,res)=>{
         errors:{},
     };
 
-    let {filename: activity_image}=req.file;
+    // let {filename: activity_image}=req.file;
 
-    let {activity_name,activity_datestart, activity_dateend,activity_pettype,activity_location,activity_time,activity_decription, activity_notice,activity_notice2} = req.body;
+    let {activity_name,activity_datestart, activity_dateend,activity_pettype,activity_location,activity_time,activity_decription, activity_notice,activity_notice2,activity_image} = req.body;
 
     if(!activity_name || activity_name.length<1){
         output.errors.activity_name = '請輸入正確的活動名稱';
@@ -136,7 +142,8 @@ router.get("/edit/:activity_id",async(req,res)=>{
     res.render("activity-edit",{...row,referer});
 });
 
-router.put("/edit/:activity_id",upload.single("activity_image"),async(req,res)=>{
+
+router.put("/edit/:activity_id",upload.none(),async(req,res)=>{
     // return res.json(req.body);
 
     const output = {
@@ -152,9 +159,9 @@ router.put("/edit/:activity_id",upload.single("activity_image"),async(req,res)=>
         return res.json(output);  //API不要用轉向
     }
 
-    const {activity_name,activity_datestart,activity_dateend,activity_pettype,activity_location,activity_time,activity_decription,activity_notice,activity_notice2} = req.body;
+    const {activity_name,activity_datestart,activity_dateend,activity_pettype,activity_location,activity_time,activity_decription,activity_notice,activity_notice2,activity_image} = req.body;
 
-    const {filename: activity_image}=req.file;
+    // const {filename: activity_image}=req.file;
 
     if(!activity_name || activity_name.length<1){
         output.errors.activity_name = '請輸入正確的活動名稱';
@@ -229,8 +236,31 @@ router.post('/addForm', upload.none(), async(req, res)=>{
     res.json(output);
   })
 
+//活動狀態
+router.put('/AformState/:activityform_id', upload.none(), async(req, res)=>{ 
+    const output = {
+        success:false,
+        postData:req.params.activityform_id,
+        code:0,
+        errors:{},
+    };
+    // console.log(output);
+    // return res.json(output)
+
+    const {activityform_id } = req.params
+
+    const sql = "UPDATE `activityform` SET `activityform_state`=0 WHERE `activityform_id`=?";
+
+    const [result] = await db.query(sql,[activityform_id])
+
+    output.result = result;
+    output.success = !! result.changedRows;
+
+    res.json(output);
+  })
+
 //取得該會員的所有預約活動紀錄
-router.get("/activitysignupform/:mid",async(req,res)=>{
+router.get("/ActivityRecord/:mid",async(req,res)=>{
     const output = {
         success:false,
         postData:req.body,
@@ -252,7 +282,7 @@ router.get("/activitysignupform/:mid",async(req,res)=>{
 })
 
 //取得某一筆預約活動的明細
-router.get("/activitysignupformlist/:activityform_id",async(req,res)=>{
+router.get("/DetailActivityRecord/:activityform_id",async(req,res)=>{
     const output = {
         success:false,
         postData:req.body,
